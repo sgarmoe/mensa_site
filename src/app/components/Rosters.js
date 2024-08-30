daf60ba9052cfa80fc5e7be89a4140350d6e5bcb
Samuel Garmoe <sam.garmoe@gmail.com>
2024-08-29 21:14:23 -0400

    working with react

--- src/app/components/Rosters.js
+++ src/app/components/Rosters.js
@@ -2,38 +2,22 @@
 
 
 const league_id = 1045634813593706496; 
-const url = `https://api.sleeper.app/v1/league/1045634813593706496/rosters`
-console.log(url);
+const url = 'https://api.sleeper.app/v1/league/1045634813593706496'
 
-const rosterRes = fetch(`https://api.sleeper.app/v1/league/1045634813593706496/rosters`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if (data.length > 0 ) {
-            console.log(data[0]);
-        } else {
-            console.log("No data found");
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-
-
-
-
-const url1 = 'http://worldtimeapi.org/api/timezone/America/New_York'
 
 async function getData() {
-    const response = await fetch(url1)
-    console.log(response)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error. Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    process.exit(1);
+  }
+
 }
 getData()
-        
-
-
-
-    
-    
 
