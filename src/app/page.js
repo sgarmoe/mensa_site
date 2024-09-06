a78adcdd93192e152740a97a2d91ac7a54768241
Samuel Garmoe <sam.garmoe@gmail.com>
2024-09-05 21:36:23 -0400

    css early edits

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 /* import LikeButton from '.like-button';*/
+import "./globals.css";
 
  
 function Header({ title }) {
@@ -6,11 +7,11 @@ function Header({ title }) {
 }
  
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+  const names = ['Insert Team names here'];
  
   return (
     <div>
-      <Header title="Develop. Preview. Ship." />
+      <Header title="Rosters for the Minimally Entertaining Nonchildbearing Sport Advocates (MENSA) Dynasty Football League!" />
       <ul>
         {names.map((name) => (
           <li key={name}>{name}</li>
