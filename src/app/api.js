b12ac37bebd64f321b9b89660c265667bf7edd1c
Samuel Garmoe <sam.garmoe@gmail.com>
2024-09-14 16:44:29 -0400

    set up mongo db of nfl players

--- src/app/api.js
+++ src/app/api.js
@@ -1,55 +1,41 @@
 //MONGO DB SETUP 
 
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const axios = require('axios');
-
 
 const uri = "mongodb+srv://samgarmoe:RMNh3YV1GOiHouua@cluster0.lu9fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-
-//choosing which data points are saved into mongo
-const playerSchema = new mongoose.Schema({ 
-  player_id: {type: Number, required: true},
-  fantasy_positions: {type: String, required: true},
-  search_full_name: {type: String, required: true}
-}) 
-
-const Player = mongoose.model('Player', playerSchema);
-
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function run() {
   try {
-  //connect to mongoDB using mongoose
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  //connect to mongoDB 
+    await client.connect();
     console.log("Connected to MongoDB");
 
     //this is the one to use once a day maximum
-    await fetchAndStoreNFLData(); // CALL TO SLEEPER FOR ALL NFL DATA
+    //await fetchAndStoreNFLData(); // CALL TO SLEEPER FOR ALL NFL DATA
 
+    //verify that data made it to mongo
     await listDatabases();
   } finally {
     // Ensures that the client will close when you finish/error
-    await mongoose.connection.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
 
 
-async function listDatabases() {
-    const admin = mongoose.connection.db.admin();
-    const databasesList = await admin().listDatabases();
+async function listDatabases(client) {
+    
+    const databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases: ");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
@@ -60,11 +46,22 @@ async function fetchAndStoreNFLData() {
         const response = await axios.get('https://api.sleeper.app/v1/players/nfl');
         const playerData = response.data;
 
-        await Player.deleteMany({});
+      if (typeof playerData === 'object' && playerData !== null) {
+        const db = client.db('nfl_data');
+        const collection = db.collection('nfl_players');
+      
+
+
+        await collection.deleteMany({});
         console.log("Initial player data cleared");
-        await Player.insertMany({});
+
+        await collection.insertMany(Object.values(playerData));
         console.log("Player data inserted");
+      } else {
+        console.log("No player data found/not in expexted format");
+      }
+
     } catch(error) {
-        console.error('Did not fetch or store data: ');
+        console.error('Did not fetch or store data: ', error);
     }
 }
