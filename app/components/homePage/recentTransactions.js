77a4ba257995c68c248c6c51cf607b539817c1bb
Samuel Garmoe <sam.garmoe@gmail.com>
2025-08-01 12:56:36 -0400

    starting map of transactions to rosters and users

--- app/components/homePage/recentTransactions.js
+++ app/components/homePage/recentTransactions.js
@@ -1,4 +1,5 @@
 //convert transactions into readable info
 
-import { displayPlayerNames, fetchUserTeamNames } from "../../backend/api";
+import { displayPlayerNames, fetchUserTeamNames, fetchCurrentRosters } from "../../backend/api";
 import { fetchTransactions } from "../../backend/lib/fetchTransactions";
+
