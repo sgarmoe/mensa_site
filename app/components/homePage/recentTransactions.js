fa3f585f287af116e451b3f123cd8552871f6b5d
Samuel Garmoe <sam.garmoe@gmail.com>
2025-08-05 11:48:57 -0400

    processing transactions from sleeper to react

--- app/components/homePage/recentTransactions.js
+++ app/components/homePage/recentTransactions.js
@@ -5,9 +5,19 @@ import axios from 'axios';
 
 import { displayPlayerNames, fetchUserTeamNames, fetchCurrentRosters } from "../../backend/api.js";
 import { fetchTransactions } from "../../backend/lib/fetchTransactions.js";
+import { connectToDatabase } from '../../backend/utils/mongoClient.js';
+
+
+async function getUsers() {
+    const db = await connectToDatabase();
+    console.log("Connected to DB:", db.databaseName);
+    const users = await db.collection('users').find().toArray();
+    return users;
+}
 
 export async function matchTransactionsToPlayers() {
     try {
+        const db = await connectToDatabase();
         const transactions = await fetchTransactions();
         console.log("Ran fetch transactions");
         console.log(transactions[0]);
@@ -24,4 +34,5 @@ export async function matchTransactionsToPlayers() {
     }
 }
 
+//getUsers();
 matchTransactionsToPlayers();
