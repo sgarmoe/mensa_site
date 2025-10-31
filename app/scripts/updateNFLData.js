27a3b3e6d851016402921cf142600e9b200e1078
Samuel Garmoe <sam.garmoe@gmail.com>
2025-10-31 15:45:04 -0400

    fixing mongo connection

--- app/scripts/updateNFLData.js
+++ app/scripts/updateNFLData.js
@@ -1,21 +1,20 @@
 // scripts/updateNFLData.js
-import { MongoClient, ServerApiVersion } from 'mongodb';
 import { fetchAndStoreNFLData } from '../backend/api.js';
-import { connectToDatabase } from '../backend/utils/mongoClient.js';
+import { connectToDatabase, client } from '../backend/utils/mongoClient.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 
 
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
 async function run() {
   try {
