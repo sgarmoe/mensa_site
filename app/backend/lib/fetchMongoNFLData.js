5319568bfc0ad2c52c7ab51212a5e44258bcc488
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2025-11-24 10:40:30 -0500

    removing testing statements

--- app/backend/lib/fetchMongoNFLData.js
+++ app/backend/lib/fetchMongoNFLData.js
@@ -22,13 +22,13 @@ export async function getPlayerById(playerId) {
 
 //fetch many players by array 
 export async function getPlayersByArray(playerIds) {
-    console.log("Entered get players by array");
+    //console.log("Entered get players by array");
     try {
         if (!Array.isArray(playerIds) || playerIds.length === 0) return [];
-        console.log("array is populated");
+        //console.log("array is populated");
 
             const normalizedIds = playerIds.map(String);
-            console.log("normalized ids: " + normalizedIds);
+            //console.log("normalized ids: " + normalizedIds);
             const db = await connectToDatabase();
             const collection = db.collection('nfl_players');
 
@@ -38,7 +38,7 @@ export async function getPlayersByArray(playerIds) {
                 { projection: { full_name: 1, position: 1, team: 1, player_id: 1, _id: 0 } }
             )
             .toArray();
-            console.log("players: " + players);
+            //console.log("players: " + players);
         return players; 
     } catch (error) {
         console.log("Error fetching players w array of IDs: " , error);
