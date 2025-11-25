799453192f763796f53e57044e642204091fdc47
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2025-11-25 11:54:57 -0500

    processing tx data from sleeper

--- app/backend/utils/processTransactions.js
+++ app/backend/utils/processTransactions.js
@@ -1,79 +1,86 @@
 import { fetchTransactions } from "../lib/fetchSleeperData.js";
 import { getPlayersByArray } from "../lib/fetchMongoNFLData.js";
 
-export async function processTransactions(playerIds) {
+export async function processTransactions() {
     const rawData = await fetchTransactions();
-    //console.log(rawData);
-    const players = getPlayersByArray(playerIds);
+    const playerIds = new Set();
+
+    rawData.forEach(tx => {
+        if (tx.adds) {
+            Object.keys(tx.adds).forEach(id => playerIds.add(id));
+        }
+        if (tx.drops) {
+            Object.keys(tx.drops).forEach(id => playerIds.add(id));
+        }
+    });
+
+    const idArray = [...playerIds];
+
+    const players = await getPlayersByArray(idArray);
 
     const processedData = rawData.map(tx => {
         if (tx.type == "trade") {
             return formatTrade(tx, players);
         }
 
-        if (tx.type == "drop" || tx.type == "add") {
+        if (tx.type == "waiver") {
             return formatFreeAgent(tx, players);
         }
 
         return null;
     });
-    //console.log(processedData);
+
+    console.log("Transaction data: ", processedData);
     return processedData.filter(Boolean);
 }
 
 function formatTrade(tx, players) {
     console.log("Entered trade format ");
-    const adds = [];
-    const drops = [];
+    
+    const adds = Object.entries(tx.adds || {}).map(([playerId, teamId]) => ({
+        player: players[playerId]?.full_name || "Unknown player", 
+        toTeam: teamId
+    }));
 
-    for (const [playerId, teamId] of Object.entries(tx.adds || {} )) {
-        adds.push({ 
-            player: players[playerId]?.full_name || "unknown player",
-            toTeam: teamId
-        });
-    }
-
-    for (const [playerId, teamId] of Object.entries(tx.drops || {})) {
-        drops.push({
-            player: players[playerId]?.full_name || "Unknown player",
-            fromTeam: teamId
-        });
-    }
+    const drops = Object.entries(tx.drops || {}).map(([playerId, teamId]) => ({
+        player: players[playerId]?.full_name || "Unknown Player",
+        fromTeam: teamId
+    }));
+    
 
     return {
         type: "trade",
-        transactionId: tx.transactionId, 
-        timestamp: tx.timestamp,
+        transactionId: tx.transaction_id, 
+        timestamp: tx.created,
+        team: tx.roster_ids?.[0]|| null,
         adds,
         drops
     };
 }
 
 function formatFreeAgent(tx, players) {
-    console.log("Entered free agent format");
-    const info = [];
+    
+    const adds = Object.entries(tx.adds || {}).map(([playerId, teamId]) => ({
+        player: players[playerId]?.full_name || "Unknown player", 
+        toTeam: teamId
+    }));
 
-    for (const [playerId, teamId] of Object.entries(tx.adds || {})) {
-        info.push({
-            type: "add",
-            team: teamId,
-            player: players[playerId]?.full_name || "Unknown player",
-            timestamp: tx.created
-        });
-    }
+    const drops = Object.entries(tx.drops || {}).map(([playerId, teamId]) => ({
+        player: players[playerId]?.full_name || "Unknown Player",
+        fromTeam: teamId
+    }));
+    
 
-    for (const [playerId, teamId] of Object.entries(tx.drops || {})) {
-        info.push({
-            type: "drop", 
-            team: teamId,
-            player: players[playerId]?.full_name || "Unknown Player",
-            timestamp: tx.created
-        });
-    }
+    return {
+        type: "waiver",
+        transactionId: tx.transaction_id, 
+        timestamp: tx.created,
+        team: tx.roster_ids?.[0]|| null,
+        adds,
+        drops
+    };
 
-    return info;
+
 }
-
-
 
 processTransactions();
