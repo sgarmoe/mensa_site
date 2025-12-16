b4802d741d1cf37b593bc91c4b445a57efbcc336
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2025-12-16 11:16:40 -0500

    setting up sleeper fetching to account for prior seasons

--- app/backend/lib/fetchSleeperData.js
+++ app/backend/lib/fetchSleeperData.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import '../config/loadEnv.js';
 
+const BASE_URL = "https://api.sleeper.app/v1";
 const leagueId = process.env.LEAGUE_ID;
 const uri = process.env.MONGODB_URI;
 const roster_url = 'https://api.sleeper.app/v1/league/1180198267141128192/rosters'
 const users_url = 'https://api.sleeper.app/v1/league/1180198267141128192/users'
 const players_url = 'https://api.sleeper.app/v1/players/nfl';
 const transactions_url = 'https://api.sleeper.app/v1/league/1180198267141128192/transactions/11'
-//const matchups_url = `https://api.sleeper.app/v1/league/${leagueId}/matchups/${week}`
 
 
 //fetch users and their team names 
-export async function fetchUserTeamNames() {
+export async function fetchUserTeamNames(leagueId) {
   try {
-    const response = await axios.get(users_url);
+    const response = await axios.get(`${BASE_URL}/leaguea/${leagueId}/users`);
     return response.data;
   } catch (error) {
     console.error("Error fetching league's users", error);
