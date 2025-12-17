5a7f4dfb77c98437efd98e6af35aa97bde0460fc
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2025-12-17 10:25:48 -0500

    fixing .env loading issues

--- app/backend/config/currentLeague.js
+++ app/backend/config/currentLeague.js
@@ -1,7 +1,12 @@
+import "./loadEnv.js";
 import { CURRENT_SEASON } from "./currentSeason.js";
 import { LEAGUES } from "./leagues.js";
 
 export const CURRENT_LEAGUE_ID = LEAGUES[CURRENT_SEASON];
+
+console.log(CURRENT_SEASON);
+console.log(LEAGUES)
+console.log("Current ID: ", CURRENT_LEAGUE_ID);
 
 if (!CURRENT_LEAGUE_ID) {
     throw new Error(`No league ID configured for ${CURRENT_SEASON}`);
--- app/backend/config/leagues.js
+++ app/backend/config/leagues.js
@@ -1,5 +1,5 @@
 export const LEAGUES = {
-    2023: process.env.LEAGUE_ID_2022,
-    2024: process.env.LEAGUE_ID_2023,
-    2025: process.env.LEAGUE_ID_2024
+    2023: process.env.LEAGUE_ID_2023,
+    2024: process.env.LEAGUE_ID_2024,
+    2025: process.env.LEAGUE_ID_2025
 };
