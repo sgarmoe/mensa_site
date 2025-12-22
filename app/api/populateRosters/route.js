47300673849a70e27a0bf483a60ed49b645e9198
Samuel Garmoe <sam.garmoe@gmail.com>
2025-12-22 17:15:26 -0500

    debugging route

--- app/api/populateRosters/route.js
+++ app/api/populateRosters/route.js
@@ -18,6 +18,7 @@ export async function GET(request) {
       );
     }
     
+    console.log("Value of year: ");
     const populatedRosters = await populateAllRosters(year);
     console.log("Route reached");
 
