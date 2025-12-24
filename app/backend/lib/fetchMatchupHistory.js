268f4a457e15665e667888e472dcb5f01c891219
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2025-12-24 11:02:49 -0500

    working to fetch historical sleeper data

--- app/backend/lib/fetchMatchupHistory.js
+++ app/backend/lib/fetchMatchupHistory.js
@@ -8,4 +8,33 @@
 // for loop to index thru LEAGUES to get each year
 //match league ID to year 
 //call for matchups w each league Id
+import '../config/loadEnv.js';
+import { LEAGUES } from "../config/leagues.js";
+import { fetchAllMatchups } from "./fetchSleeperData.js";
 
+export async function fetchPastSeasonData() {
+
+    const allWeeks = week ? [week] : Array.from({ length : 14 }, (_, i) => i + 1); 
+
+    const seasons = Object.entries(LEAGUES)
+        .filter(([, leagueId]) => Boolean(leagueId))
+        .map(([year, leagueId]) => ({
+            year: Number(year),
+            leagueId
+        }))
+        .sort((a, b) => a.year - b.year);
+
+    //console.log(LEAGUES);
+    //console.log(seasons);
+    const results = [];
+
+    for (const season of seasons) {
+        //console.log(season.leagueId, week);
+        const seasonData = await fetchAllMatchups(season.leagueId, allWeeks); 
+        results.push(seasonData);
+    }
+    console.log(results[10]);
+    return results;
+}
+
+fetchPastSeasonData();
