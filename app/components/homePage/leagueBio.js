6e1193a871fe613ff099279ce98f41c523e3d6e1
Samuel Garmoegit config --global user.name Samuel <sam.garmoe@gmail.com>
2026-02-11 16:08:07 -0500

    further home page adds

--- app/components/homePage/leagueBio.js
+++ app/components/homePage/leagueBio.js
@@ -1,21 +1,22 @@
 //component for league bio on home page
 
 import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
 
 export default function Bio() {
     return (
         <Grid container spacing = {10}>
             <Grid 
             size={8} 
-            sx = {{
-                borderRight: 'var(--Grid-borderWidth solid',
-                borderColor: 'divider',
-            }}>
+            >
                 <p1>
                 Testing first grid <br/>
                 Testing if lines are skipped 
-                </p1>
+                </p1>   
             </Grid>
+
+             <Divider orientation="vertical" flexItem sx={{ mx: -5 }} />
+
             <Grid size={4}>
                 Testing second grid
             </Grid>
