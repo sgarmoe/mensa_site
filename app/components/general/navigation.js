958a3ebe2d9de59fd9c5fe47745dbd66abef57b2
Samuel Garmoe <sam.garmoe@gmail.com>
2026-02-12 22:15:05 -0500

    remove fluff

--- app/components/general/navigation.js
+++ app/components/general/navigation.js
@@ -4,7 +4,10 @@ import "../../globals.css";
 import { Stack, Divider, Button } from '@mui/material';
 
 //TODO
-//
+//template for bg of each page
+//use a light gray maybe?
+//start w home page, go one by one 
+
 
 export default function Header() {
   return (
