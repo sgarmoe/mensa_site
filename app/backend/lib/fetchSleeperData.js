245f4d2b12de1048f89f50d01d132f18b754a64a
Samuel Garmoe <sam.garmoe@gmail.com>
2026-05-10 18:43:59 -0400

    created axios client w timeout

--- app/backend/lib/fetchSleeperData.js
+++ app/backend/lib/fetchSleeperData.js
@@ -1,50 +1,48 @@
 import axios from 'axios';
 
-const BASE_URL = "https://api.sleeper.app/v1";
+const sleeperClient = axios.create({
+    baseURL: "https://api.sleeper.app/v1",
+    timeout: 10000,
+});
 
-//fetch users and their team names 
 export async function fetchUserTeamNames(leagueId) {
-  return axios 
-    .get(`${BASE_URL}/league/${leagueId}/users`)
+  return sleeperClient
+    .get(`/league/${leagueId}/users`)
     .then(res => res.data);
 }
 
-//fetch current league's rosters from Sleeper
 export async function fetchCurrentRosters(leagueId) {
-  return axios
-    .get(`${BASE_URL}/league/${leagueId}/rosters`)
+  return sleeperClient
+    .get(`/league/${leagueId}/rosters`)
     .then(res => res.data);
 }
 
-//fetch all FA and trades transactions 
-export async function fetchTransactions (leagueId, week){ 
-    return axios
-      .get(`${BASE_URL}/league/${leagueId}/transactions/${week}`)
-      .then(res => res.data); 
+export async function fetchTransactions(leagueId, week) {
+    return sleeperClient
+      .get(`/league/${leagueId}/transactions/${week}`)
+      .then(res => res.data);
 }
 
-//can use to fetch prior years
 export async function fetchAllMatchups(leagueId, week) {
-      return axios
-        .get(`${BASE_URL}/league/${leagueId}/matchups/${week}`)
+    return sleeperClient
+        .get(`/league/${leagueId}/matchups/${week}`)
         .then(res => res.data);
 }
 
-//fetch league details per season
 export async function fetchSpecificLeagueSettings(leagueId) {
-    return axios
-      .get(`${BASE_URL}/league/${leagueId}`)
+    return sleeperClient
+      .get(`/league/${leagueId}`)
       .then(res => res.data);
 }
 
 export async function fetchWinnerPlayoffBracket(leagueId) {
-  return axios
-    .get(`${BASE_URL}/league/${leagueId}/winners_bracket`)
+  return sleeperClient
+    .get(`/league/${leagueId}/winners_bracket`)
     .then(res => res.data);
 }
 
 export async function fetchLoserPlayoffBracket(leagueId) {
-  return axios
-    .get(`${BASE_URL}/league/${leagueId}/losers_bracket`)
+  return sleeperClient
+    .get(`/league/${leagueId}/losers_bracket`)
     .then(res => res.data);
 }
