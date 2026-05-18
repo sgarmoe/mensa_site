9ba1b3685939440185b297a6770e72042c77a114
Samuel Garmoe <sam.garmoe@gmail.com>
2026-05-18 07:06:06 -0400

    bug in mongo data fetch

--- app/backend/lib/fetchMongoNFLData.js
+++ app/backend/lib/fetchMongoNFLData.js
@@ -1,17 +1,12 @@
 import { connectToDatabase } from "../config/mongoClient.js";
 
-const uri = process.env.MONGODB_URI;
-const roster_url = 'https://api.sleeper.app/v1/league/1180198267141128192/rosters'
-const users_url = 'https://api.sleeper.app/v1/league/1180198267141128192/users'
-const players_url = 'https://api.sleeper.app/v1/players/nfl';
-
 //fetch single player
 export async function getPlayerById(playerId) {
     try {
         const db = await connectToDatabase();
         const collection = db.collection('nfl_players');
         const player = await collection.findOne(
-            { playerid: String(playerId) },
+            { player_id: String(playerId) },
             { projection: { full_name: 1, position: 1, team: 1, player_id: 1, _id: 0 } }
         );
         return player;
