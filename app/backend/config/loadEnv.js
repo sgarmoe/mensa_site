e7aa970787250b3f203cd52dabdc98f5014c6bed
Samuel Garmoe <sam.garmoe@gmail.com>
2026-08-01 10:42:47 -0400

    clarify contents to self

--- app/backend/config/loadEnv.js
+++ app/backend/config/loadEnv.js
@@ -4,8 +4,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Recreate __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); //asgn abs path of env to filename
+const __dirname = path.dirname(__filename); //get abs path of env asgn to dirname
 
 // Load .env from the project root
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
